78c43c85fa74c43a4e99c0aed2f0459092059a9b
Bohdan Honcharuk <dariydragon26@gmail.com>
2020-07-25 20:42:25 +0300

    testing

--- lib/int_datetime.dart
+++ lib/int_datetime.dart
@@ -4,21 +4,21 @@ extension datetime on int {
   DateTime get toDateTime => DateTime.fromMillisecondsSinceEpoch(this);
 }
 
-int get dtNow => DateTime.now().millisecondsSinceEpoch;
+int dtNow() => DateTime.now().millisecondsSinceEpoch;
 
-int get dtDay {
+int dtDay() {
   final now = DateTime.now();
 
   return DateTime.utc(now.year, now.month, now.day).millisecondsSinceEpoch;
 }
 
-int get dtMonth {
+int dtMonth() {
   final now = DateTime.now();
 
   return DateTime.utc(now.year, now.month).millisecondsSinceEpoch;
 }
 
-int get dtYear {
+int dtYear() {
   final now = DateTime.now();
 
   return DateTime.utc(now.year).millisecondsSinceEpoch;
